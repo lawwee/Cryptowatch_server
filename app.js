7a19a8f4cdc58f39be9106fd23ca210a15c31c85
lawwee <lawalmohammed567@gmail.com>
2022-04-18 15:12:53 +0100

    fixed exposed mongo url

--- app.js
+++ app.js
@@ -4,6 +4,9 @@ const bodyparser = require('body-parser');
 
 const authRoutes = require('./routes/auth');
 
+const MONGODB_URL = 
+    `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@cluster0.vuuxv.mongodb.net/${process.env.MONGO_DEFAULT_DATABASE}`;
+
 const app = express();
 
 app.use((req, res, next) => {
@@ -28,9 +31,7 @@ app.use((error, req, res, next) => {
     res.status(status).json({ message: message, data: data});
 });
 
-mongoose.connect(
-    'mongodb+srv://Lawwee:U2sXBZLOOni4lymR@cluster0.vuuxv.mongodb.net/cryptowatch?retryWrites=true&w=majority'
-    )
+mongoose.connect(MONGODB_URL)
     .then(result => {
         app.listen(8080);
     })
